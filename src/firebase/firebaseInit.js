6bdd143d18e453b0ea1c74c13faea167cf2fa2dd
dbae82 <dbae82@gmail.com>
2022-03-21 11:09:23 -0500

    build: firebase config and env variables set

---
+++ src/firebase/firebaseInit.js
@@ -0,0 +1,17 @@
+import firebase from "firebase/app";
+import "firebase/firestore"
+
+const firebaseConfig = {
+    apiKey: process.env.VUE_APP_APIKEY,
+    authDomain: process.env.VUE_APP_AUTHDOMAIN,
+    projectId: process.env.VUE_APP_PROJECTID,
+    storageBucket: process.env.VUE_APP_STORAGEBUCKET,
+    messagingSenderId: process.env.VUE_APP_SENDERID,
+    appId: process.env.VUE_APP_APPID
+};
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const timestamp = firebase.firestore.FieldValue.serverTimestamp;
+
+export { timestamp };
+export default firebaseApp.firestore();
